5ec7787feefd9277349dd0e6e87a962df9b53ac6
Huw <HuwSy@users.noreply.github.com>
2023-01-17 18:46:10 +0000

    Cleanup notes

--- Angular2+/sharepoint-choice/src/lib/sharepoint-choice.utils.ts
+++ Angular2+/sharepoint-choice/src/lib/sharepoint-choice.utils.ts
@@ -1,26 +1,34 @@
 import pnp from '@pnp/pnpjs';
 import * as MSAL from "@azure/msal-browser";
-import {App} from './App';
+import { Logger, LogLevel } from "@pnp/logging";
+import { PnPLogging } from './PnPLogging';
+import { App } from './App';
 
+///<summary>
+/// This is to be used in place of specific pnp.sp function when using these form fields to aid in data transforms and a few other fringe cases outlined in the method coments 
+///</summary>
 export class SharepointChoiceUtils {
+    // context can be read and updated
     public context:string = '';
 
+    // attempt to establish correct context url for the site from one of the available sources then setup logging for this class
     constructor(
-        c: string
+        context?: string
     ) {
-        if ((c || "") == "") {
-          if (typeof window['_spPageContextInfo'] == "object") {
-            this.context = window['_spPageContextInfo']['webAbsoluteUrl'];
-          } else {
-            this.context = document.location.href.split('?')[0].split('#')[0].split('/_layouts/')[0].split('/Lists/')[0].split('/Pages/')[0].split('/SitePages/')[0];
-          }
-        } else {
-          this.context = c;
-        }
+      this.context = context;
+      if ((this.context || "") == "")
+        this.context = typeof window['_spPageContextInfo'] == "object" ? window['_spPageContextInfo']['webAbsoluteUrl'] : null;
+      if ((this.context || "") == "")
+        this.context = document.location.href.split('?')[0].split('#')[0].split('/_layouts/')[0].split('/Lists/')[0].split('/Pages/')[0].split('/SitePages/')[0];
 
-        pnp.sp.setup({sp:{baseUrl:this.context}});
+      this.context = this.context.replace(/\/$/,'');
+
+      pnp.sp.setup({sp:{baseUrl:this.context}});
+      Logger.subscribe(new PnPLogging());
+      Logger.activeLogLevel = LogLevel.Warning;
     }
 
+    // get the current user and permissions to a flat object for easier use in [disabled]="permission['']" etc
     public async permissions():Promise<any> {
         var p = {}, u = 0;
 
@@ -42,11 +50,12 @@ export class SharepointChoiceUtils {
         return {userId: u, perms: p}
     }
     
-    public async fields(list):Promise<any> {
+    // get list fields in the appropriate format for use in <sharepoint-choice spec=""> attributes
+    public async fields(listTitle:string):Promise<any> {
         var spec = {};
 
         try {
-            var arr = await pnp.sp.web.lists.getByTitle(list).fields.get();
+            var arr = await pnp.sp.web.lists.getByTitle(listTitle).fields.get();
             arr.forEach(x => {
                 spec[x.InternalName] = x;
                 spec[x.InternalName].Context = this.context;
@@ -58,47 +67,51 @@ export class SharepointChoiceUtils {
         return spec;
     }
 
-    public param(q:string):string {
-        var rx = new RegExp(`[?&]${q}=([^&]+).*$`);
-        var returnVal = document.location.search.match(rx);
-        return returnVal === null ? null : returnVal[1];
-    }
-
-    public async data(id:number, list):Promise<any> {
+    // load list item data and parse any data types appropriate for use in <sharepoint-choice ngModel=""> attributes
+    public async data(id:number, listTitle:string):Promise<any> {
         var d = {};
 
         try {
-          d = await pnp.sp.web.lists.getByTitle(list).items.getById(id).get();
+          d = await pnp.sp.web.lists.getByTitle(listTitle).items.getById(id).get();
           for (var key in d) {
             // people fields return twice
             if (key.endsWith('StringId') && (d[key.replace(/StringId$/,'Id')] || d[key.replace(/StringId$/,'Id')] === null))
               delete d[key];
+
             // if there are attachments start loading
             if (key == 'Attachments') {
               if (d[key] === true)
-                d[key] = { results: await pnp.sp.web.lists.getByTitle(list).items.getById(id).attachmentFiles() };
+                d[key] = { results: await pnp.sp.web.lists.getByTitle(listTitle).items.getById(id).attachmentFiles() };
               else
                 d[key] = { results:[] };
             }
+
             // extract metadata for save
             if (key == 'odata.type')
               d['__metadata'] = {type:d[key]};
+
             // remove odata. prefixed
             if (key.startsWith('odata.'))
               delete d[key];
+
             // dont process nulls
             if (!d[key])
               continue;
-            // parse objects
+
+          // parse objects within text fields for looped data
             try {
-              if (d[key].toString().trim().substring(0,1) == '{' || d[key].toString().trim().substring(0,1) == '[') {
+              let f = d[key].toString().trim().substring(0,1);
+              if ((f == '{' || f == '[') && d[key].toString().trim().endsWith(f)) {
                 d[key] = JSON.parse(d[key]);
                 d[key] = this.parseLoop(d[key]);
                 continue;
               }
             } catch (e) {}
-            // dates
-            if (d[key].toString().match(/[1920]{2}[0-9]{2}\-[01][0-9]\-[0-3][0-9]/) != null) {
+
+            // dates and date times
+            let i = d[key].toString();
+            if (i.match(/^[1920]{2}[0-9]{2}\-[01][0-9]\-[0-3][0-9][ T][0-2][0-9]:[0-5][0-9]:*[0-9]*\.*[0-9]*Z*$/) != null
+              || i.match(/^[1920]{2}[0-9]{2}\-[01][0-9]\-[0-3][0-9]$/) != null) {
               d[key] = new Date(d[key]);
               continue;
             }
@@ -114,7 +127,8 @@ export class SharepointChoiceUtils {
     private parseLoop(i:any):any {
       try {
         if (typeof i == "string") {
-          if (i.match(/20[0-9]{2}\-[01][0-9]\-[0-3][0-9]/) != null) {
+          if (i.match(/^[1920]{2}[0-9]{2}\-[01][0-9]\-[0-3][0-9][ T][0-2][0-9]:[0-5][0-9]:*[0-9]*\.*[0-9]*Z*$/) != null
+            || i.match(/^[1920]{2}[0-9]{2}\-[01][0-9]\-[0-3][0-9]$/) != null) {
             return new Date(i);
           }
         } else if (typeof i == "object") {
@@ -127,16 +141,13 @@ export class SharepointChoiceUtils {
       return i;
     }
   
-    public async history(id:number, list):Promise<any> {
-        return await pnp.sp.web.lists.getByTitle(list).items.getById(id).versions.get();
-    }
-
-    public async msalApi(clientId: string, tokenRole: string, endPoint: string, release: string):Promise<any> {
-      // connect client
+    // calls an api more generically
+    public async callApi(tenancyOnMicrosoft: string, clientId: string, permissionScope: string, apiUrl: string, httpMethod: string, jsonPostData: any):Promise<any> {
+      // client settings
       var config = {
         auth: {
             clientId: clientId,
-            authority: `https://login.microsoftonline.com/${App.Tenancy}.onmicrosoft.com`,
+            authority: `https://login.microsoftonline.com/${tenancyOnMicrosoft}.onmicrosoft.com`,
             redirectUri: this.context.replace(/\/$/,'')
         },
         cache: {
@@ -145,13 +156,16 @@ export class SharepointChoiceUtils {
         }
       }
       
+      // init client
       var msal = new MSAL.PublicClientApplication(config);
       
+      // permission settings
       var params = {
-        scopes: [`${App.Token}/${release}/${tokenRole}`],
+        scopes: [permissionScope],
         account: msal.getAllAccounts()[0]
       };
 
+      // attempt to get token or login and get token
       var login;
       try {
         login = await msal.acquireTokenSilent(params);
@@ -162,21 +176,25 @@ export class SharepointChoiceUtils {
       }
 
       // query api
-      var r = await fetch(release == 'Local'
-          ? `https://localhost/${endPoint}`
-          : `${App.Token}/${release}/${endPoint}`, {
-              method: 'GET',
+      var r = await fetch(apiUrl, {
+              method: httpMethod,
               headers: {
-                  'Authorization': `Bearer ${login.accessToken}`
-              }
+                  'Authorization': `Bearer ${login.accessToken}`,
+                  'Content-Type': 'application/json'
+              },
+              body: jsonPostData ? JSON.stringify(jsonPostData) : null,
           });
       
       // return formatted data
       return await r.json();
     }
 
-    public async save(form, uned, list):Promise<number> {
+    // patch save list item data and parse any data types appropriate for use in <sharepoint-choice ngModel=""> attributes
+    public async save(formDataIncIdToUpdate: any, uneditedDataToBuildPatch: any, listTitle: string):Promise<number> {
+        let form = formDataIncIdToUpdate, uned = uneditedDataToBuildPatch;
         var save = JSON.parse(JSON.stringify(form));
+        if (uned == null)
+          uned = {};
 
         try {
           delete save["$$hashKey"];
@@ -204,10 +222,10 @@ export class SharepointChoiceUtils {
           
           // save/update the item
           if (typeof save.Id == "undefined" || save.Id < 1) {
-            var saving = await pnp.sp.web.lists.getByTitle(list).items.add(save);
+            var saving = await pnp.sp.web.lists.getByTitle(listTitle).items.add(save);
             save.Id = saving.data.Id;
           } else {
-            await pnp.sp.web.lists.getByTitle(list).items.getById(save.Id).update(save);
+            await pnp.sp.web.lists.getByTitle(listTitle).items.getById(save.Id).update(save);
           }
     
           // process attachments as deletes then uploads
@@ -219,7 +237,7 @@ export class SharepointChoiceUtils {
             });
 
             for (var i = 0; i < deletes.length; i++)
-              await pnp.sp.web.lists.getByTitle(list).items.getById(save.Id).attachmentFiles.getByName(deletes[i]).delete();
+              await pnp.sp.web.lists.getByTitle(listTitle).items.getById(save.Id).attachmentFiles.getByName(deletes[i]).delete();
     
             var adds = form.Attachments.results.filter(a => {
               return !a.Deleted && !a.ServerRelativeUrl
@@ -231,7 +249,7 @@ export class SharepointChoiceUtils {
             });
 
             if (adds.length > 0)
-              await pnp.sp.web.lists.getByTitle(list).items.getById(save.Id).attachmentFiles.addMultiple(adds);
+              await pnp.sp.web.lists.getByTitle(listTitle).items.getById(save.Id).attachmentFiles.addMultiple(adds);
           }
         } catch (e) {
           alert('Error saving');
@@ -239,5 +257,12 @@ export class SharepointChoiceUtils {
         }
 
         return save.Id;
+    }
+
+    // get query parameters, not strictly sharepoint but reused a lot
+    public param(parameterToReturn:string):string {
+        var rx = new RegExp(`[?&]${parameterToReturn}=([^&]+).*$`);
+        var returnVal = document.location.search.match(rx);
+        return returnVal === null ? null : returnVal[1];
     }
 }
