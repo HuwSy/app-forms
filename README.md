ab6566a894252fa329a89863472fd0331da3bfd3
HuwSy <3321353+HuwSy@users.noreply.github.com>
2024-05-28 21:02:57 +0100

    Restructured and Angular18

--- README.md
+++ README.md
@@ -1,7 +1,131 @@
 # app-forms
 
-A simple Angular framework for rapid development of SharePoint hosted and SharePoint data stored apps. This allows for list views, submission forms, task forms, emails, approvals, audit etc. 
+A simple Angular framework for rapid development originally in AngularJS/JavaScript and progressed to Angular18 within an SPFx wrapper giving controls which can use this.spec to determin the field details and manipulate this.form
 
-This was originally an AngularJS/JavaScript coded, simplified version of Nintex/PowerPlatform but based on the now deprecated AngularJS, due to when the work began. It requires a content web part or script editor web part or a modern equivelent of these. As there were so many deprecated requirements here then it is uploaded as is with unlikely/minimal further development to the AngularJS version but may be of use for rapid prototyping or fall back interfaces.
+To be added to app module
+```
+import { SharepointChoiceModule } from 'sharepoint-choice';
+```
+```
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    SharepointChoiceModule
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+```
 
-The Angular2+ version has now matured using a newer versions of Angular (16 currently) within an SPFx package.
+To be added to html templates
+```
+<app-choice [form]="form" [spec]="spec" [field]="'Title'"></app-choice>
+```
+
+To be added to the components
+```
+import { SharepointChoiceUtils } from 'sharepoint-choice';
+```
+```
+// register the utils
+this.util = new SharepointChoiceUtils(this.context);
+
+// load user and permission details
+this.util.permissions().then(r => {
+    this.userId = r.userId;
+    this.perm = r.perms;
+});
+
+// load the list field spec
+this.util.fields(this.list).then(r => {
+    this.spec = r;
+});
+
+// load the list item
+this.util.data(id, this.list).then(d => {
+    this.form = d;
+    this.uned = JSON.parse(JSON.stringify(this.form));
+});
+
+// load version history
+this.util.history(id, this.list).then(d => {
+    this.versions = d
+});
+
+// save the form
+this.util.save(this.form, this.uned, this.list).then(id => {
+    this.form.Id = id;
+});
+
+// load an api, or post to an api
+this.util.callApi(
+    `guid`,
+    `permission`,
+    'path',
+    App.APIRelease || App.Release,
+    'POST',
+    {'content':'dummy'}).then(results => {
+    this.results = results;
+});
+```
+
+Dependencies
+```
+  "dependencies": {
+    "@angular/animations": "^18.0.0",
+    "@angular/common": "^18.0.0",
+    "@angular/compiler": "^18.0.0",
+    "@angular/core": "^18.0.0",
+    "@angular/elements": "^18.0.0",
+    "@angular/forms": "^18.0.0",
+    "@angular/platform-browser": "^18.0.0",
+    "@angular/platform-browser-dynamic": "^18.0.0",
+    "@angular/router": "^18.0.0",
+    "@pnp/pnpjs": "^2.15.0",
+    "sharepoint-choice": "^4.0.0",
+    "rxjs": "~7.8.1",
+    "tslib": "^2.6.2",
+    "zone.js": "~0.14.3"
+  },
+  "devDependencies": {
+    "@angular-devkit/build-angular": "^18.0.1",
+    "@angular/cli": "^18.0.1",
+    "@angular/compiler-cli": "^18.0.0",
+    "@types/jasmine": "~5.1.0",
+    "jasmine-core": "~5.1.0",
+    "typescript": "~5.4.2"
+  }
+```
+
+To be added to tsconfig.json to avoid some errors
+```
+  "strict": false,
+  "skipLibCheck": true,
+```
+
+To add to package.json for easier use of the wrapper
+```
+  "bundle": "ng build --aot --build-optimizer --delete-output-path --output-hashing none"
+```
+
+To generate SSL certs for debugging within localhost and SPFx wrapper
+```
+npm install -g office-addin-dev-certs
+office-addin-dev-certs install --days 3650
+```
+
+Which will need this to be added to angular.json "serve":
+```
+  "development": {
+    "browserTarget": "<solution>:build:development",
+    "publicHost": "localhost",
+    "port": 443,
+    "ssl": true,
+    "disableHostCheck": true
+  }
+},
+"options": {
+  "sslKey": "./localhost.key",
+  "sslCert": "./localhost.crt"
+}
+```
---
+++ sharepoint-choice/src/lib/sharepoint-choice.utils.ts
@@ -0,0 +1,312 @@
+import pnp from '@pnp/pnpjs';
+import * as MSAL from "@azure/msal-browser";
+import { Logger, LogLevel } from "@pnp/logging";
+import { PermissionKind } from "@pnp/sp/security";
+import { PnPLogging } from './PnPLogging';
+import { App } from './App';
+
+///<summary>
+/// This is to be used in place of specific pnp.sp function when using these form fields to aid in data transforms and a few other fringe cases outlined in the method coments 
+///</summary>
+export class SharepointChoiceUtils {
+    // context can be read and updated
+    public context?:string = '';
+
+    // attempt to establish correct context url for the site from one of the available sources then setup logging for this class
+    constructor(
+        context?: string
+    ) {
+      this.context = context;
+      let w:any = window;
+      if ((this.context || "") == "")
+        this.context = typeof w['_spPageContextInfo'] == "object" ? w['_spPageContextInfo']['webAbsoluteUrl'] : undefined;
+      if ((this.context || "") == "")
+        this.context = document.location.href.split('?')[0].split('#')[0].split('/_layouts/')[0].split('/Lists/')[0].split('/Pages/')[0].split('/SitePages/')[0];
+
+      this.context = this.context?.replace(/\/$/,'');
+
+      pnp.sp.setup({sp:{baseUrl:this.context}});
+      Logger.subscribe(new PnPLogging());
+      Logger.activeLogLevel = LogLevel.Warning;
+    }
+
+    // get the current user and permissions to a flat object for easier use in [disabled]="permission['']" etc
+    // NOTE: this will only detect direct assignments or users added to a mail enabled global security group
+    public async permissions():Promise<any> {
+        var p:any = {}, u = 0;
+
+        try {
+          var user = await pnp.sp.web.currentUser.get();
+          u = user.Id;
+          var web = await pnp.sp.web.get();
+          var webTitle = web.Title;
+          var perm = await pnp.sp.web.currentUser.groups.get();
+          perm.forEach(x => {
+            p[x.LoginName] = true;
+            if (x.LoginName.startsWith(`${webTitle} `))
+              p[x.LoginName.replace(`${webTitle} `,'')] = true;
+          })
+        } catch (e) {
+          p = {Error: true};
+        }
+
+        return {userId: u, perms: p}
+    }
+
+    // check permission against object
+    public async hasPermission(object:any, permissions:any[PermissionKind]):Promise<boolean> {
+      try {
+        var perm = await object.getCurrentUserEffectivePermissions();
+        for (var p in permissions) {
+          if (pnp.sp.web.hasPermissions(perm, permissions[p]))
+            return true;
+        }
+      } catch (e) {}
+      return false;
+    }
+    
+    // get list fields in the appropriate format for use in <sharepoint-choice spec=""> attributes
+    public async fields(listTitle:string):Promise<any> {
+        var spec:any = {'odata.metadata': this.context};
+
+        try {
+            var arr = await pnp.sp.web.lists.getByTitle(listTitle).fields.get();
+            arr.forEach(x => {
+                spec[x.InternalName] = x;
+                spec[x.InternalName].Context = this.context;
+            });
+        } catch (e) {
+            spec['Title'] = {TypeAsString:'Text',MaxLength:16,Description:'Tooltip'};
+        }
+
+        return spec;
+    }
+
+    // load list item data and parse any data types appropriate for use in <sharepoint-choice ngModel=""> attributes
+    public async data(id:number, listTitle:string):Promise<any> {
+        var d:any = {};
+
+        try {
+          d = await pnp.sp.web.lists.getByTitle(listTitle).items.getById(id).get();
+          for (var key in d) {
+            // people fields return twice
+            if (key.endsWith('StringId') && (d[key.replace(/StringId$/,'Id')] || d[key.replace(/StringId$/,'Id')] === null))
+              delete d[key];
+
+            // if there are attachments start loading
+            if (key == 'Attachments') {
+              if (d[key] === true)
+                d[key] = { results: await pnp.sp.web.lists.getByTitle(listTitle).items.getById(id).attachmentFiles() };
+              else
+                d[key] = { results:[] };
+            }
+
+            // extract metadata for save
+            if (key == 'odata.type')
+              d['__metadata'] = {type:d[key]};
+
+            // remove odata. prefixed
+            if (key.startsWith('odata.'))
+              delete d[key];
+
+            // dont process nulls
+            if (!d[key] || d[key] === null)
+              continue;
+              
+            // return arrays back to results, fix pnpjs not behaving as expected
+            if (typeof d[key] == "object" && !d[key].results && d[key].length > 0) {
+              d[key] = {
+                results: d[key],
+                __metadata: {type: (typeof d[key][0] == "number" ? "Collection(Edm.Int32)" : "Collection(Edm.String)")}
+              }
+            }
+
+            // parse objects within text fields for looped data
+            try {
+              let f = d[key].toString().trim().substring(0,1);
+              if ((f == '{' || f == '[') && d[key].toString().trim().endsWith(f == '{' ? '}' : ']')) {
+                d[key] = JSON.parse(d[key]);
+                d[key] = this.parseLoop(d[key]);
+                continue;
+              }
+            } catch (e) {}
+
+            // dates and date times
+            let i = d[key].toString();
+            if (i.match(/^[1920]{2}[0-9]{2}\-[01][0-9]\-[0-3][0-9][ T][0-2][0-9]:[0-5][0-9]:*[0-9]*\.*[0-9]*Z*$/) != null
+              || i.match(/^[1920]{2}[0-9]{2}\-[01][0-9]\-[0-3][0-9]$/) != null) {
+              d[key] = new Date(d[key]);
+              continue;
+            }
+          }
+        } catch (e) {
+          alert('Error loading');
+          throw e;
+        }
+
+        return d;
+    }
+
+    private parseLoop(i:any):any {
+      try {
+        if (typeof i == "string") {
+          if (i.match(/^[1920]{2}[0-9]{2}\-[01][0-9]\-[0-3][0-9][ T][0-2][0-9]:[0-5][0-9]:*[0-9]*\.*[0-9]*Z*$/) != null
+            || i.match(/^[1920]{2}[0-9]{2}\-[01][0-9]\-[0-3][0-9]$/) != null) {
+            return new Date(i);
+          }
+        } else if (typeof i == "object") {
+          try {
+            for (var a in i)
+              i[a] = this.parseLoop(i[a]);
+          } catch (e) {}
+        } 
+      } catch (e) {}
+      return i;
+    }
+  
+    // calls an api more generically
+    public async callApi(tenancyOnMicrosoft: string, clientId: string, permissionScope: string, apiUrl?: string, httpMethod?: string, jsonPostData?: any):Promise<any> {
+      // client settings
+      var config = {
+        auth: {
+            clientId: clientId,
+            authority: `https://login.microsoftonline.com/${tenancyOnMicrosoft}.onmicrosoft.com`,
+            redirectUri: this.context?.replace(/\/$/,'')
+        },
+        cache: {
+            cacheLocation: "localStorage",
+            storeAuthStateInCookie: false
+        }
+      }
+      
+      // init client
+      var msal = new MSAL.PublicClientApplication(config);
+
+      await msal.initialize();
+      
+      // permission settings
+      var params = {
+        scopes: [permissionScope],
+        account: msal.getAllAccounts()[0]
+      };
+
+      // attempt to get token or login and get token
+      var login;
+      try {
+        login = await msal.acquireTokenSilent(params);
+      } catch (error) {
+        try {
+          await msal.loginPopup(params);
+          params.account = msal.getAllAccounts()[0];
+          login = await msal.acquireTokenSilent(params);
+        } catch (e) {
+          throw `Exception logging in to MSAL for scope ${permissionScope} with error ${e}`;
+        }
+      }
+
+      // if no url, login only, then return
+      if (apiUrl == null)
+        return null;
+
+      // query api
+      var r;
+      try {
+        r = await fetch(apiUrl, {
+              method: httpMethod || 'GET',
+              headers: {
+                  'Authorization': `Bearer ${login.accessToken}`,
+                  'Content-Type': 'application/json'
+              },
+              body: jsonPostData ? JSON.stringify(jsonPostData) : null,
+          });
+      
+        // return formatted data for 2xx, 4xx and 5xx will not return
+        if (r.status == 204)
+          return null;
+        return await r.clone().json();
+      } catch (e) {
+        throw `Exception getting API data with status ${r?.status} response ${await r?.text()}`;
+      }
+    }
+
+    // patch save list item data and parse any data types appropriate for use in <sharepoint-choice ngModel=""> attributes
+    public async save(formDataIncIdToUpdate: any, uneditedDataToBuildPatch: any, listTitle: string):Promise<number> {
+        let form = formDataIncIdToUpdate, uned = uneditedDataToBuildPatch;
+        var save = JSON.parse(JSON.stringify(form));
+        if (!uned || uned == null)
+          uned = {};
+
+        try {
+          delete save["$$hashKey"];
+
+          for (var key in save) {
+            if ((save[key] === null && uned[key] !== null) || key == "Id" || key == "__metadata")
+              continue;
+            
+            // remove and unedited, including internal fields
+            if (key == "Attachments" || ((uned[key] || uned[key] === null) && JSON.stringify(uned[key]) == JSON.stringify(save[key]))) {
+              delete save[key];
+              continue;
+            }
+
+            // convert dates
+            if (typeof save[key].toJSON != "undefined") {
+              save[key] = save[key].toJSON();
+              continue;
+            }
+
+            // convert JSON
+            if (typeof save[key] == "object" && !save[key].results && !save[key].Url)
+              save[key] = JSON.stringify(save[key]);
+            
+            // ensure no nulls selected, should never occur but does on some browsers?
+            if (typeof save[key] == "object" && save[key].results)
+              save[key].results = save[key].results.filter((i:any) => i !== null && i !== undefined);
+          }
+          
+          // save/update the item
+          if (typeof save.Id == "undefined" || save.Id < 1) {
+            var saving = await pnp.sp.web.lists.getByTitle(listTitle).items.add(save);
+            save.Id = saving.data.Id;
+          } else {
+            await pnp.sp.web.lists.getByTitle(listTitle).items.getById(save.Id).update(save);
+          }
+    
+          // process attachments as deletes then uploads
+          if (form.Attachments && form.Attachments.results && form.Attachments.results.length > 0) {
+            var deletes = form.Attachments.results.filter((a:any) => {
+              return a.Deleted
+            }).map((a:any) => {
+              return a.FileName;
+            });
+
+            for (var i = 0; i < deletes.length; i++)
+              await pnp.sp.web.lists.getByTitle(listTitle).items.getById(save.Id).attachmentFiles.getByName(deletes[i]).delete();
+    
+            var adds = form.Attachments.results.filter((a:any) => {
+              return !a.Deleted && !a.ServerRelativeUrl
+            }).map((a:any) => {
+              return {
+                name: a.FileName,
+                content: a.Data
+              };
+            });
+
+            if (adds.length > 0)
+              await pnp.sp.web.lists.getByTitle(listTitle).items.getById(save.Id).attachmentFiles.addMultiple(adds);
+          }
+        } catch (e) {
+          alert('Error saving');
+          throw e;
+        }
+
+        return save.Id;
+    }
+
+    // get query parameters, not strictly sharepoint but reused a lot
+    public param(parameterToReturn:string):string|undefined {
+        var rx = new RegExp(`[?&]${parameterToReturn}=([^&]+).*$`);
+        var returnVal = document.location.search.match(rx);
+        return returnVal === null ? undefined : returnVal[1];
+    }
+}
