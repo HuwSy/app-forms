a081392c4fa5a88ef457872fcf68149ffa81c7e1
Huw <HuwSy@users.noreply.github.com>
2025-02-05 20:40:16 +0000

    Update sharepoint-choice.logging.ts

--- sharepoint-choice/src/lib/sharepoint-choice.logging.ts
+++ sharepoint-choice/src/lib/sharepoint-choice.logging.ts
@@ -9,10 +9,13 @@ export class SharepointChoiceLogging implements ErrorHandler {
 
     public async handleError(error: any): Promise<void> {
         let w:any = window;
-        console.error(w._spPageContextInfo.userLoginName);
-        console.error(w._spPageContextInfo.webAbsoluteUrl);
-        console.error(w._spPageContextInfo.webTitle);
+        if (w._spPageContextInfo) {
+            console.error(w._spPageContextInfo.userLoginName);
+            console.error(w._spPageContextInfo.webAbsoluteUrl);
+            console.error(w._spPageContextInfo.webTitle);
+        }
         console.error(App.Release);
+        console.error(document.location.href);
         console.error(document.location.search);
         console.error(document.location.hash);
         try {
