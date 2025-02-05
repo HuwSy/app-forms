e2abe634325e471696b5afa0c3f546b18e76e2a4
Huw <HuwSy@users.noreply.github.com>
2025-02-05 20:34:56 +0000

    Update PnPLogging.ts

--- sharepoint-choice/src/lib/PnPLogging.ts
+++ sharepoint-choice/src/lib/PnPLogging.ts
@@ -7,8 +7,17 @@ export class PnPLogging implements ILogListener {
     }
 
     public async log(entry: any): Promise<void> {
-        console.error(App.AppName);
+        let w:any = window;
+        console.error(w._spPageContextInfo.userLoginName);
+        console.error(w._spPageContextInfo.webAbsoluteUrl);
+        console.error(w._spPageContextInfo.webTitle);
         console.error(App.Release);
-        console.error(entry);
+        console.error(document.location.search);
+        console.error(document.location.hash);
+        try {
+            console.error(JSON.stringify(await fromError(entry, {offline: true})));
+        } catch (e) {
+            console.error(JSON.stringify(entry.data ? entry.data.StackTrace : null));
+        }
     }
 }
