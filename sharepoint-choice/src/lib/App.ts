ee4fa4965016226f4f90f141e5476ebd883fbaab
Huw <HuwSy@users.noreply.github.com>
2025-07-16 05:21:53 +0000

    Update App.ts

--- sharepoint-choice/src/lib/App.ts
+++ sharepoint-choice/src/lib/App.ts
@@ -1,13 +1,59 @@
+var release = ~document.location.href.toLowerCase().indexOf('/app-') || ~document.location.href.toLowerCase().indexOf('prd/') ? 'LIVE'
+    : ~document.location.href.toLowerCase().indexOf('/pre-') || ~document.location.href.toLowerCase().indexOf('pre/') ? 'PRE'
+      : ~document.location.href.toLowerCase().indexOf('/tst-') || ~document.location.href.toLowerCase().indexOf('tst/') ? 'TST'
+        : ~document.location.href.toLowerCase().indexOf('/sit-') || ~document.location.href.toLowerCase().indexOf('sit/') ? 'SIT'
+          : 'DEV';
+
 export const App = {
-    AppName: 'SharePoint-Choice',
-    Category: 'SPO',
-    Entity: 'All',
-    Release: ~document.location.href.toLowerCase().indexOf('/app-') ? 'LIVE' 
-        : ~document.location.href.toLowerCase().indexOf('/pre-') ? 'PRE' 
-        : ~document.location.href.toLowerCase().indexOf('/tst-') ? 'TST' 
-        : ~document.location.href.toLowerCase().indexOf('/sit-') ? 'SIT' 
-        : 'DEV',
-    Token: '',
-    Tenancy: '',
-    GraphClient: ''
+  Release: release,
+
+  Tenancy: '',
+  GraphClient: "",
+
+  Grafana: {
+    "Hostname": document.location.origin,
+    "Category": 'SPO',
+    "Environment": release,
+    "Username": undefined,
+    "UserAgent": navigator.userAgent,
+    "Language": navigator.language,
+  },
+
+  ApiServers: {
+    'grafana': {
+      LIVE: 'https://grafanaprd/loki/api/v1/push',
+      PRE: 'https://grafanapre/loki/api/v1/push',
+      TST: 'https://grafanatst/loki/api/v1/push',
+      SIT: 'https://grafanasit/loki/api/v1/push',
+      DEV: 'https://grafanadev/loki/api/v1/push',
+    },
+    'common': {
+      LIVE: 'https://apiprd',
+      PRE: 'https://apipre',
+      TST: 'https://apitst',
+      SIT: 'https://apisit',
+      DEV: 'https://apidev',
+    }
+  },
+  ApiToken: {
+    'common': {
+      LIVE: 'api://API.',
+      PRE: 'api://API-Pre.',
+      TST: 'api://API-Tst.',
+      SIT: 'api://API-Sit.',
+      DEV: 'api://API-Dev.',
+    }
+  },
+  ApiMap: {
+    'test': {
+      LIVE: '',
+      PRE: '',
+      TST: '',
+      SIT: '',
+      DEV: '',
+      name: 'Test',
+      port: 44301,
+      server: 'common'
+    }
+  }
 }
