cd4e678ef5c4417a6c3c26787171d3ffb3a6a3e6
Huw <HuwSy@users.noreply.github.com>
2025-12-05 17:51:42 +0000

    Initial internal filtered sortable table

--- sharepoint-choice/src/public-api.ts
+++ sharepoint-choice/src/public-api.ts
@@ -1,3 +1,4 @@
 export * from './lib/sharepoint-choice.component';
+export * from './lib/sharepoint-choice.table';
 export * from './lib/sharepoint-choice.utils';
 export * from './lib/sharepoint-choice.logging';
