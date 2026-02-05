f243ebd676e3a9b7d98ea1f835b18d0139498dd4
Huw <HuwSy@users.noreply.github.com>
2026-02-05 12:40:10 +0000

    Update public-api.ts

--- sharepoint-choice/src/public-api.ts
+++ sharepoint-choice/src/public-api.ts
@@ -3,3 +3,4 @@ export * from './lib/sharepoint-choice.table';
 export * from './lib/sharepoint-choice.utils';
 export * from './lib/sharepoint-choice.logging';
 export * from './lib/sharepoint-choice.models';
+export * from './lib/sharepoint-choice.refresh';
